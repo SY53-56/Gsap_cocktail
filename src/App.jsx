72a48e3cbb4e60b23d44a5e28e9d7c5e0e6115e6
sahul-yadav <yadavsahul220@gmail.com>
2025-08-14 12:16:18 +0530

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { ScrollTrigger , SplitText } from 'gsap/all'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+  <div className='flex-center'>
+      <h1 className=' text-3xl'>sahul</h1>
+    <p className=' text-'></p>
+  </div>
+    </>
+  )
+}
+
+export default App
