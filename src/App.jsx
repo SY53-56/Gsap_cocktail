148cc7ce3bf66f058ce73036b86208e5f3f51cc6
sahul-yadav <yadavsahul220@gmail.com>
2025-08-15 13:05:30 +0530

    implement Hero scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import { ScrollTrigger , SplitText } from 'gsap/all'
 import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
     <>
-  <div className='flex-center'>
-      <h1 className=' text-3xl'>sahul</h1>
-    <p className=' text-'></p>
-  </div>
+    <Navbar/>
+    <Hero/>
+   
     </>
   )
 }
